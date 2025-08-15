a09ac68cc423ce3218ba22913365b06bb6fdc47d
davifodagamer <davi.monareto@escola.pr.gov.br>
2025-08-15 09:35:32 -0300

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-// cria referência ao form e ao elemento H3
-const frm = document.querySelector("form")
-const resp = document.querySelector (h3)
-
-frm.addEventListener("submit", (e) => {
-    const nome = frm.inNome.value  //obtém o nome digitado no form
-    resp.innerText = 'Olá, ${Davi}' // exibe a resposta do programa
-    e.preventDefault() // evita envio do from
-})
